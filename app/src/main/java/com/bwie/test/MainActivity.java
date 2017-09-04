ad4698a8c9c8aee2141a21e67be382728efc22a9
TigerIs <328304262@qq.com>
2017-09-04 10:22:59 +0800

    第二次提交

--- app/src/main/java/com/bwie/test/MainActivity.java
+++ app/src/main/java/com/bwie/test/MainActivity.java
@@ -9,7 +9,7 @@ import android.widget.Button;
 public class MainActivity extends AppCompatActivity {
 
     private MyView myView;
-    private Button chance;
+    private Button chance,jia,jian;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +17,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         myView = (MyView) findViewById(R.id.myview);
         chance = (Button) findViewById(R.id.chacecolor);
+        jia = (Button) findViewById(R.id.jia);
+        jian = (Button) findViewById(R.id.jian);
         addanim();
         chance.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -24,9 +26,29 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+        jia.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                jiaaddanim();
+            }
+        });
     }
 
     private void addanim() {
+        ObjectAnimator anim = ObjectAnimator.ofFloat(myView, "rotation", 0f, 360f);
+        anim.setDuration(10000);
+        anim.setRepeatCount(Integer.MAX_VALUE);
+        anim.start();
+    }
+
+    private void jiaaddanim() {
+        ObjectAnimator anim = ObjectAnimator.ofFloat(myView, "rotation", 0f, 100f, 200, 280, 360f);
+        anim.setDuration(10000);
+        anim.setRepeatCount(Integer.MAX_VALUE);
+        anim.start();
+    }
+
+    private void jianaddanim() {
         ObjectAnimator anim = ObjectAnimator.ofFloat(myView, "rotation", 0f, 360f);
         anim.setDuration(10000);
         anim.setRepeatCount(Integer.MAX_VALUE);
